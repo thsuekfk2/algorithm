a08e346b5e3aac70226642e9f9636070687bd2fd
lee hyebin <76821963+thsuekfk2@users.noreply.github.com>
2023-08-06 17:46:03 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,12 +1,16 @@
 //문자열 배열 s1과 s2가 주어질 때 같은 원소의 개수를 return
 function solution(s1, s2) {
-    let count=0;
-    for(let i=0; i<=s1.length-1; i++){
-        for(let j=0; j<=s2.length-1; j++){
-            if(s1[i]===s2[j]){
-                count++;
-            }
-        }
-    }
-    return count
+    // let count=0;
+    // for(let i=0; i<=s1.length-1; i++){
+    //     for(let j=0; j<=s2.length-1; j++){
+    //         if(s1[i]===s2[j]){
+    //             count++;
+    //         }
+    //     }
+    // }
+    // return count
+
+    //리팩토링 버전
+    const intersection = s1.filter((x) => s2.includes(x));
+    return intersection.length;
 }
