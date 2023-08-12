06e67fa9baf9e38e34e1fe86f6f53129dc90fdef
lee hyebin <76821963+thsuekfk2@users.noreply.github.com>
2023-08-12 17:43:08 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,12 +1,18 @@
-//정수 배열 numbers 에서 두개를 곱해서 만들 수 있는 최댓값 리턴
+// 정수 배열 numbers 에서 두개를 곱해서 만들 수 있는 최댓값 리턴
+// function solution(numbers) {
+//     let maxNum = -100000000;
+//     for(let i=0; i<=numbers.length-1; i++){
+//         for(let j=0; j<=numbers.length-1; j++){
+//             if(i != j && maxNum < numbers[i] * numbers[j]){
+//                 maxNum = numbers[i] * numbers[j];
+//             }
+//         }
+//     }
+//     return maxNum;
+// }
+
+//리팩토링, 정렬을 이용하여 풀이한 방식
 function solution(numbers) {
-    let maxNum = -100000000;
-    for(let i=0; i<=numbers.length-1; i++){
-        for(let j=0; j<=numbers.length-1; j++){
-            if(i != j && maxNum < numbers[i] * numbers[j]){
-                maxNum = numbers[i] * numbers[j];
-            }
-        }
-    }
-    return maxNum;
+    let sortArray = numbers.sort((a, b) => a - b);
+    return Math.max(numbers[0]*numbers[1], numbers[numbers.length-1]*numbers[numbers.length-2]);
 }
