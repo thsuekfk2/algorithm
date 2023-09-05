a9d12ad82cfd8d2891fd2e73f2e8b50c8e9a29cd
lee hyebin <76821963+thsuekfk2@users.noreply.github.com>
2023-09-06 02:02:36 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,13 +1,12 @@
 function solution(d, budget) {
     let count = 0;
-    d.sort((a, b) => a - b);
-    
+    d.sort();
     for(let i=0; i<d.length; i++){
-        budget -= d[i]
-        if(budget < 0){
-            break;
+        //예산이 남아있다면 남은 예산에서 현재 돈을 빼줌
+        if(budget > d[i]){
+            budget -= d[i]
+        }else{
+            return i;
         }
-        count++;
     }
-    return count;
 }
