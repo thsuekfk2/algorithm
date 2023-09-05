536024c9efdaf4a7ca99b321157fa4ab343c9f9f
lee hyebin <76821963+thsuekfk2@users.noreply.github.com>
2023-09-06 02:01:57 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+    let count = 0;
+    d.sort((a, b) => a - b);
+    
+    for(let i=0; i<d.length; i++){
+        budget -= d[i]
+        if(budget < 0){
+            break;
+        }
+        count++;
+    }
+    return count;
+}
