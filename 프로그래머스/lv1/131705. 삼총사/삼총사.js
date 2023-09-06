d3987b6c1c31a636126dd8aa4481b72aa414a5df
lee hyebin <76821963+thsuekfk2@users.noreply.github.com>
2023-09-06 16:33:33 +0900

    [level 1] Title: 삼총사, Time: 0.31 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,27 +1,23 @@
+function getCombination(number,num) {
+    let result = [];
 
-const getCombinations=(arr, count)=> {
-  const combinations = [];
+    const combination = (current, start) => {
+        if (current.length === num) {
+			result.push(current.slice());
+            return;
+        }
 
-  function combine(current, start) {
-    if (current.length === count) {
-      combinations.push(current.slice()); // 배열을 복사하여 추가
-      return;
+        for (let i = start; i < number.length; i++) {
+            combination([...current, number[i]], i + 1);
+        }
     }
-
-    for (let i = start; i < arr.length; i++) {
-      current.push(arr[i]);
-      combine(current, i + 1);
-      current.pop(); // 다음 경우를 위해 원소 제거
-    }
-  }
-
-  combine([], 0);
-  return combinations;
+    combination([], 0);
+    return result;
 }
 
 function solution(number) {
     let count = 0;
-    const combinations = getCombinations(number,3);
+    const combinations = getCombination(number,3);
     combinations.map((data)=>{
         if(data.reduce((a,c)=>a+c,0)===0){
             count++;
