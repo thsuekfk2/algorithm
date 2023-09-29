e19c2ed08e07a84b42fee686dd2072d28291c572
lee hyebin <76821963+thsuekfk2@users.noreply.github.com>
2023-09-29 23:59:51 +0900

    [unrated] Title: 문자열 나누기, Time: 3.82 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -0,0 +1,23 @@
+function solution(s) {
+  let answer = 0;
+  let fixedChar = "";
+  let cnt1 = 0;
+  let cnt2 = 0;
+
+  for (let c of s) {
+    if (!fixedChar) fixedChar = c;
+    
+    if(fixedChar === c) cnt1++;
+    else cnt2++;
+
+    if (cnt1 === cnt2) {
+        answer++;
+        cnt1 = 0;
+        cnt2 = 0;
+        fixedChar = "";
+    }
+  }
+  if (fixedChar) answer++;
+
+  return answer;
+}
