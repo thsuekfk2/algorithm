e774d1d6c6941b71a55213bd928cbb1721101028
lee hyebin <76821963+thsuekfk2@users.noreply.github.com>
2023-11-11 16:17:54 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,17 +1,19 @@
+//x축에서 가장 작은 값 구하기
+//y축에서 가장 큰 값 구하기
 function solution(wallpaper) {
-    let arrX = [];
-    let arrY = [];
+    let arr = [];
     
     for(let i=0; i<= wallpaper.length; i++){
         for(let j=0; j<= wallpaper[0].length; j++){
             if(wallpaper[i] && wallpaper[i][j]==="#"){
-                arrX.push(i)
-                arrY.push(j)
+                arr.push([i,j])
             }
         }
     }
-    let sortX = arrX.sort((a,b)=>a-b)
-    let sortY = arrY.sort((a,b)=>a-b)
-    
-    return [sortX[0], sortY[0], sortX[sortX.length-1]+1, sortY[sortY.length-1] +1]
+    console.log(arr)
+    let sortArr = arr.sort((a,b)=>a[0]-b[0])
+    if(sortArr[0].length === 1){
+        return [1, 0, 2, 1];
+    }
+    console.log(sortArr[0], sortArr[sortArr[0].length])
 }
