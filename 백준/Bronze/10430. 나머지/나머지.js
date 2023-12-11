03df91ec4c7a6fe3c93dfca9fe362fe2f92bf171
lee hyebin <76821963+thsuekfk2@users.noreply.github.com>
2023-12-11 10:37:08 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,13 @@
+//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
+
+let A = input[0];
+let B = input[1];
+let C = input[2];
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
