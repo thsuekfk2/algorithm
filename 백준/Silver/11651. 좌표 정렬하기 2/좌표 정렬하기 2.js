35a6c38d4c70142f97a8895fcc113e03958f536d
lee hyebin <76821963+thsuekfk2@users.noreply.github.com>
2023-12-12 10:39:11 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 576 ms, Memory: 62132 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let [T, ...testCase] = input;
+
+let arr = [];
+let results = "";
+
+for (let i = 0; i < T; i++) {
+  let dev = testCase[i].split(" ");
+  arr.push(dev);
+}
+
+arr
+  .sort((a, b) => {
+    if (a[1] !== b[1]) {
+      return Number(a[1]) - Number(b[1]);
+    }
+    return Number(a[0]) - Number(b[0]);
+  })
+  .forEach((coords) => {
+    results += `${coords[0]} ${coords[1]}\n`;
+  });
+
+console.log(results);
