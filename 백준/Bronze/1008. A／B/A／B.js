087863f4cdb436111427541b849a0a5a1104a4f3
lee hyebin <76821963+thsuekfk2@users.noreply.github.com>
2024-01-13 16:04:56 +0900

    [Bronze V] Title: A/B, Time: 120 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,4 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split(" ");
+
+console.log(input[0] / input[1]);
