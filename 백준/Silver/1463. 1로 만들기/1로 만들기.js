b5ca812bb142b144a0e9caa5c3ce1a4a8ec3c58b
lee hyebin <thsuekfk2@naver.com>
2024-05-06 18:23:23 +0900

    [Silver III] Title: 1로 만들기, Time: 240 ms, Memory: 41060 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,29 +1,31 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n").map(Number);
-
+// 1463
 // X가 3으로 나누어 떨어지면, 3으로 나눈다.
 // X가 2로 나누어 떨어지면, 2로 나눈다.
 // 1을 뺀다.
+// 정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 연산을 사용하는 횟수의 최솟값을 출력하시오.
 
-let num = input[0];
-let dp = Array.from({ length: num + 1 }).fill(0);
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
+let input = Number(fs.readFileSync(filePath).toString());
 
-//dp[1] = 0
-//dp[2] = dp[1]+1 = 1
+let dp = [];
 
-//크기 비교 후 작은것으로 넣기
-//dp[3] = dp[2]+1 = 2
-//dp[3] = dp[3 / 3] + 1 = dp[1] + 1 = 1
+dp[0] = 0;
+dp[1] = 0;
+dp[2] = 1;
+dp[3] = 1;
 
-for (let i = 2; i <= num; i++) {
+for (let i = 4; i <= input; i++) {
+  //1을 뺀 값
   dp[i] = dp[i - 1] + 1;
-  if (i % 2 === 0) {
-    dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
-  }
+  //3으로 나누어 떨어지면, 3으로 나눈 값과 1뺀 값과 비교
   if (i % 3 === 0) {
-    dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  }
+  //2로 나누어 떨어지면, 2로 나눈 값과 1뺀 값과 비교
+  if (i % 2 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
   }
 }
 
-console.log(dp[num]);
+console.log(dp[input]);
