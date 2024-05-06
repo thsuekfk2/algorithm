99679b26b78c5b465d4dfad8fb3852ff6b8886e2
lee hyebin <thsuekfk2@naver.com>
2024-05-06 19:28:44 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 192 ms, Memory: 9656 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -1,17 +1,19 @@
+// 11727
+//2×n 직사각형을 1×2, 2×1과 2×2 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
-let input = fs.readFileSync(filePath).toString().trim();
-const num = Number(input);
+let input = Number(fs.readFileSync(filePath).toString());
 
-//2×n 직사각형을 1×2, 2×1과 2×2 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
-//첫째 줄에 2×n 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지를 출력한다.
-
-let dp = new Array(num + 1).fill(0);
+let dp = [];
 
 dp[1] = 1;
 dp[2] = 3;
+dp[3] = 5;
+dp[4] = 11;
 
-for (let i = 3; i <= num; i++) {
+for (let i = 5; i <= input; i++) {
   dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
 }
-console.log(dp[num]);
+
+console.log(dp[input]);
