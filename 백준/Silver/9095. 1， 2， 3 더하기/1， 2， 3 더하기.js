ad6ce9207658b09cb087c6beeaed2b3552334296
lee hyebin <thsuekfk2@naver.com>
2024-05-07 11:50:40 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 128 ms, Memory: 9360 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -1,9 +1,22 @@
+// 정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
+// (n은 양수이며 11보다 작다.)
+
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let [T, ...input] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((data) => +data);
 
-// 정수 4를 1, 2, 3의 합으로 나타내는 방법은 총 7가지가 있다. 합을 나타낼 때는 수를 1개 이상 사용해야 한다.
+let dp = [];
 
+dp[0] = 0;
+dp[1] = 1; // 1
+dp[2] = 2; // 1+1, 2
+dp[3] = 4; // 1+2, 2+1, 1+1+1, 3
+dp[4] = 7;
 // 1+1+1+1
 // 1+1+2
 // 1+2+1
@@ -11,25 +24,11 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 // 2+2
 // 1+3
 // 3+1
-// 정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
-
-let [T, ...testCase] = input;
-let index = 0;
-
-//dp배열 초기화
-let dp = new Array(T + 1);
-
-dp[0] = 0;
-dp[1] = 1;
-dp[2] = 2;
-dp[3] = 4;
-dp[4] = 7;
 
 for (let i = 5; i <= 11; i++) {
   dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
 }
 
-while (T--) {
-  let dpIndex = Number(testCase[index++]);
-  console.log(dp[dpIndex]);
+for (let i = 0; i < T; i++) {
+  console.log(dp[input[i]]);
 }
