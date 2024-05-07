1b26b776ac9e249716a7907177f31acc51eef119
lee hyebin <thsuekfk2@naver.com>
2024-05-07 12:00:40 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1368 ms, Memory: 247040 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -1,8 +1,13 @@
+// N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+let [T, ...input] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((data) => +data);
 
-let [_, ...testCase] = input;
-let sortArr = testCase.sort((a, b) => a - b);
-
-console.log(sortArr.join("\n"));
+const sortArray = input.sort((a, b) => a - b);
+console.log(sortArray.join("\n"));
