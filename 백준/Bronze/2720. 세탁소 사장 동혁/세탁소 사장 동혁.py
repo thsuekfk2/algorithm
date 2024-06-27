9f6b19dea50928d4eb686d507b767d32cffe93fe
lee hyebin <thsuekfk2@naver.com>
2024-06-27 15:16:37 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,27 +1,13 @@
-
 N = int(input())
-x1=0
-x2=0
-x3=0
-x4=0
 
-for i in range(N):
+for _ in range(N):
   x = int(input())
-  while x>0:
-    if x//25 > 0:
-      x-=25
-      x1+=1
-    elif x//10 > 0:
-      x-=10
-      x2+=1
-    elif x//5> 0:
-      x-=5
-      x3+=1
-    elif x//1> 0:
-      x-=1
-      x4+=1
-  print(x1,x2,x3,x4)
-  x1=0
-  x2=0
-  x3=0
-  x4=0
+  denominations = [25, 10, 5, 1]
+  counts = [0, 0, 0, 0]
+
+  for i in range(4):
+    while x >= denominations[i]:
+      x -= denominations[i]
+      counts[i] += 1
+
+  print(*counts)
